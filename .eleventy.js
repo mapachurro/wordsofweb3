10ec37b07343be474845a2113e949c39a9945c57
mapachurro <oliver.renwick@consensys.net>
2024-08-26 17:52:45 -0400

    functional build without english, have to merge branch

--- .eleventy.js
+++ .eleventy.js
@@ -11,17 +11,31 @@ module.exports = function(eleventyConfig) {
     
     locales.forEach(locale => {
       const localePath = path.join(localesDir, locale);
-      const localeFile = fs.readFileSync(`${localePath}/${locale}.json`);
-      const termsData = JSON.parse(localeFile).terms;
-      
-      Object.keys(termsData).forEach(termKey => {
-        allTerms.push({
-          termKey: termKey,
-          locale: locale,
-          termData: termsData[termKey],
-        });
-      });
+      const jsonFilePath = `${localePath}/${locale}.json`;
+
+      if (fs.existsSync(jsonFilePath)) {
+        const localeFile = fs.readFileSync(jsonFilePath);
+        const termsData = JSON.parse(localeFile).terms;
+
+        if (termsData && typeof termsData === 'object') {
+          Object.keys(termsData).forEach(termKey => {
+            allTerms.push({
+              termKey: termKey,
+              locale: locale,
+              termData: termsData[termKey],
+            });
+          });
+        } else {
+          console.error(`Error: "terms" is undefined or not an object in ${jsonFilePath}`);
+        }
+      } else {
+        console.error(`Error: JSON file not found at ${jsonFilePath}`);
+      }
     });
+
+    if (allTerms.length === 0) {
+      console.error('No terms were found in the JSON files.');
+    }
 
     return allTerms;
   });
